2dc036c79cbbc50884cfd7dfbcaca1ecbd5d83db
Амайя <amaya@MacBook-Air-Amaja.local>
2026-05-01 19:53:48 +0600

    fav context

--- React-store/src/context/favorites-context.tsx
+++ React-store/src/context/favorites-context.tsx
@@ -1,2 +1,40 @@
+import React, {createContext, useContext, useEffect, useState } from "react";
 
+const FavoritesContext = createContext(undefined);
+
+export function FavoritesProvider({children} : {children: React.ReactNode}) {
+
+    const [favorites, setFavorites] = useState(() => {
+        const saved = localStorage.getItem('favorites')
+        return saved ? JSON.parse(saved) : []
+    })
+
+    useEffect(() => {
+        localStorage.setItem('favorites', JSON.stringify(favorites))
+    }, [favorites])
+
+    const addToFavorites = (id: string) => {
+        if (!favorites.includes(id)) {
+           setFavorites([...favorites, id])
+        }
+    }
+
+    const removeFromFavorites = (id: string) => {
+        setFavorites(favorites.filter((fav: string) => fav !== id));
+    }
+
+    const isFavorites= (id: string) => {
+        return favorites.includes(id);
+    }
+
+    const value = {addToFavorites, removeFromFavorites, favorites, isFavorites}
+
+    return <FavoritesContext.Provider value={value}>
+        {children}
+    </FavoritesContext.Provider>
+}
+
+export function useFavorite() {
+    return useContext(FavoritesContext);
+}
 
